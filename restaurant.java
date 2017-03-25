574ad18f5a9574c0de8a142aeba7d8a23413c008
ntuz <nat5ds@virginia.edu>
2017-03-25 18:13:38 -0400

    Update restaurant.java

--- restaurant.java
+++ restaurant.java
@@ -38,4 +38,10 @@ public class Restaurant extends HttpServlet {
 		doGet(request, response);
 	}
 
+		public static List<String> Foods = Arrays.asList("Mexicano","Italiano","Pizza","Chinese","Gushers","Go Starve");
+	public static List<String> GoOut = Arrays.asList("Yes","Yes, of course","No. Just kidding Yes","Yes you stupid idiot","Dude come on...obviously yes. ","Not sure I can help with that...JK YES");
+	public static List<String> WhatToDo = Arrays.asList("Literally Nothing","Try and spell Tuz's last name.","Go hurt Winston","Go tickle Rohan","Point and laugh at Pete","Go clap for Nic");
+	public static List<String> Movies = Arrays.asList("Caddyshack","Rohan Finds Love","Who is Winston?","Miracle","Duck Hats Forever","You don't deserve a movie");
+	public static List<String> Places = Arrays.asList("Rohan's House","Winston's Grave","Pete's Box","UVa Hackathon","Rohan's Feet","I don't wanna answer");
+	public static List<String> Presents = Arrays.asList("Rohan's socks","Winston's tears","Winston's gf","Happiness","Pete's back hair","That guy sucks...no present");
 }
